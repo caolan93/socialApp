6d25c19d32517969e7cdd1aad27344d4985c490b
caolan <caolan@digigrow.ie>
2023-04-18 20:16:34 -0400

    creating controller and route for auth route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,34 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
+
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User(...req.body, {
+      password: passwordHash,
+      viewedProfile: 0,
+      impressions: 0,
+    });
+    const savedUser = await newUser.save();
+
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({
+      error: err.messages
+    });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// ROUTES
+import authRoutes from "./routes/auth.js";
+
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -36,7 +39,10 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // ROUTES WITH FILES
-app.post("/auth/register", upload.single("picture", register));
+app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.route("/auth", authRoutes);
 
 // MONGOOSE CONNECTION
 const PORT = process.env.PORT || 6001;
